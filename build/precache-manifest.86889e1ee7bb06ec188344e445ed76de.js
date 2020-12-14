6b391213d5264b4332438e1046792520a9964377
shamim99apk <rianahmedshamim99apk@gmail.com>
2020-12-14 12:12:00 +0600

    now its okey

---
+++ build/precache-manifest.86889e1ee7bb06ec188344e445ed76de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cfb194ebd503ca51c76690ceefd965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f10fc07b84c3e41ab2",
+    "url": "/static/css/main.13820d97.chunk.css"
+  },
+  {
+    "revision": "f9cf16205a545b2808d7",
+    "url": "/static/js/2.39b21a21.chunk.js"
+  },
+  {
+    "revision": "c1f10fc07b84c3e41ab2",
+    "url": "/static/js/main.9ee7f535.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2175d7070f199a47b2cd48e63bddcc4",
+    "url": "/static/media/Bars-1s-200px.e2175d70.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
